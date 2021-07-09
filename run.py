1eecf8c4b171d90cd046e6273744dd9dcd920fd0
KSanthanesh <santhanesh_2004@yahoo.com>
2021-07-09 18:42:58 +0000

    Add initial commit

--- run.py
+++ run.py
@@ -1,1 +1,23 @@
-# python code goes here
+# Questions and Correct Answer using Dictionaries
+question = {
+    "Which Country is Known as the 'Land of Raising Sun'?": "A",
+    "Which Country is known as the 'Playground of Europe'?": "C",
+    "What percentage of the human body is water?": "B",
+    "What is the hottest chilli pepper in the world?": "A",
+    "Steel is more elastic than Rubber because:": "C",
+
+}
+
+# Multiple choice of answers using Lists
+options = [
+    ["A. Japan", "B. New Zealand", "C. Fiji", "D. China"],
+    ["A. Austria", "B. holland", "C. Switzerland", "D. Italy"],
+    ["A. 75%", "B. 60%", "C. 69%", "D. 65%"],
+    ["A.The Carolina Reaper", "B.Ghost Pepper", "C.Pot Barrackpore",
+     "D.Pot Red"],
+    ["A.its density is high", "B.it is a metal",
+     "C.ratio of stress to strain is more",
+     "D. ratio of stress to strain is less"]
+
+    ]
+print(question)
